54196a2e80a96890a4c12094420d39e772b91bd0
ko-umeda-coder <ko-umeda@halmek.co.jp>
2025-10-22 08:07:02 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -223,7 +223,7 @@ async function convertToJapanPost(csvFile, sender) {
     rowOut[1] = "0";
 
     // お届け先情報
-    rowOut[7]  = name;
+    rowOut[8]  = name;
     rowOut[9]  = postal;
     rowOut[10] = addrParts.pref;
     rowOut[11] = addrParts.city;
