478ace52ee7e64952751375a7dc4e6035ae63204
ko-umeda-coder <ko-umeda@halmek.co.jp>
2025-11-14 13:22:36 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -108,8 +108,8 @@ const waitForXLSX = () => new Promise((resolve) => {
 
     for (const r of data) {
       const name = r[13] || "";
-      const postal = cleanTelPostal(r[10] || "");
-      const addrFull = r[15] || "";   // ← フル住所1列
+      const postal = cleanTelPostal(r[11] || "");
+      const addrFull = r[12] || "";   // ← フル住所1列
       const phone = cleanTelPostal(r[14] || "");
       const orderNo = cleanOrderNumber(r[1] || "");
 
