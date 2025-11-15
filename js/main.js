d9472534f463ab101b001ff0b40b162db5a45e33
ko-umeda-coder <ko-umeda@halmek.co.jp>
2025-11-15 18:48:41 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -138,7 +138,7 @@ const waitForXLSX = () => new Promise((resolve) => {
     for (const r of data) {
       // ★ インポート元の列指定はテスト版から変更しない
       const name    = r[13] || "";                 // 宛名
-      const postal  = cleanTelPostal(r[13] || ""); // 郵便番号
+      const postal  = cleanTelPostal(r[11] || ""); // 郵便番号
       const addrRaw = r[12] || "";                 // フル住所1列
       const phone   = cleanTelPostal(r[14] || ""); // 電話番号
       const orderNo = cleanOrderNumber(r[1] || "");// ご注文番号
